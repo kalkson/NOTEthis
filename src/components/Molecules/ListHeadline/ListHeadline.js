46f61e6e31999f20ce284c2fc772132846c23703
Damian Kalka <kalka762@gmail.com>
2021-01-09 01:20:15 +0100

    implement dynamic title editing

--- src/components/Molecules/ListHeadline/ListHeadline.js
+++ src/components/Molecules/ListHeadline/ListHeadline.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import propTypes from 'prop-types';
+import ListInput from 'components/Atoms/ListInput/ListInput';
 import StyledListHeadline from './ListHeadline.styled';
 import { ReactComponent as PenSVG } from '../../../assets/vector/pen-icon.svg';
 import { ReactComponent as DeleteSVG } from '../../../assets/vector/delete-icon.svg';
 
-const ListHeadline = ({ children, className }) => (
-  <StyledListHeadline className={className}>
-    {children}
-    <PenSVG className="pen-icon" title="zmień nazwę" />
-    <DeleteSVG className="delete-icon" title="usuń" />
-  </StyledListHeadline>
-);
+const ListHeadline = ({ children, className }) => {
+  const headline = useRef(null);
+  const [headlineValue, setHeadlineValue] = useState('');
+  const [textContent, setTextContent] = useState(null);
+
+  const handlePenClick = () => {
+    setHeadlineValue(headline.current.textContent);
+    // headlineInput.current?.value = headlineValue;
+  };
+
+  const handleSubmit = (e, value) => {
+    e.preventDefault();
+    console.log(value);
+    setHeadlineValue('');
+    setTextContent(value);
+    // headline.textContent = value;
+  };
+
+  if (headlineValue.length !== 0)
+    return <ListInput value={headlineValue} handleSubmit={handleSubmit} />;
+
+  return (
+    <StyledListHeadline className={className} ref={headline}>
+      {textContent || children}
+      <PenSVG
+        className="pen-icon"
+        role="button"
+        onClick={() => handlePenClick()}
+      />
+      <DeleteSVG className="delete-icon" />
+    </StyledListHeadline>
+  );
+};
 
 ListHeadline.propTypes = {
   children: propTypes.oneOfType([
